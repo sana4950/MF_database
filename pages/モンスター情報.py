dc17d5522a7bfb7dfd2473385f9a2aa91b961cfa
sana4950 <shalulu853@gmail.com>
2023-03-10 00:34:28 +0900

    Chang5
    どこまでうまくいくか確認2

--- pages/モンスター情報.py
+++ pages/モンスター情報.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.title('モンスター情報DB')
 st.subheader('種族一覧')
 
-df=pd.read_csv('MF_database.csv',index_col='モンスター名')
+#df=pd.read_csv('MF_database.csv',index_col='モンスター名')
 
 if st.button('ピクシー種'):
     st.write('表示させるぞー！')
