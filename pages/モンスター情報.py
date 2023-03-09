2379e1f0d069e498c568afc63a21f63beb09fd5b
sana4950 <shalulu853@gmail.com>
2023-03-10 00:28:08 +0900

    Change4
    どこまでうまくいくか確認

--- pages/モンスター情報.py
+++ pages/モンスター情報.py
@@ -7,8 +7,9 @@ st.subheader('種族一覧')
 df=pd.read_csv('MF_database.csv',index_col='モンスター名')
 
 if st.button('ピクシー種'):
-    Pixies=['ピクシー','ミント','スエコ','エンジェル','リリム']
-    stock=st.selectbox(label='ピクシー種一覧',
-                       options=Pixies)
-    df_tgt=df.loc[stock]
-    st.dataframe(df_tgt)
+    st.write('表示させるぞー！')
+#    Pixies=['ピクシー','ミント','スエコ','エンジェル','リリム']
+#    stock=st.selectbox(label='ピクシー種一覧',
+#                       options=Pixies)
+#    df_tgt=df.loc[stock]
+#    st.dataframe(df_tgt)
