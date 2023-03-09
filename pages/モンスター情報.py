848a0b5e12fe7aa65eb7644f0b82d5b190a64531
sana4950 <shalulu853@gmail.com>
2023-03-10 00:50:45 +0900

    Change9
    文字コード変更でエラー解決
    データ操作元に戻す

--- pages/モンスター情報.py
+++ pages/モンスター情報.py
@@ -4,12 +4,11 @@ import pandas as pd
 st.title('モンスター情報DB')
 st.subheader('種族一覧')
 
-df=pd.read_csv('MF_database.csv',encoding="utf-8")
+df=pd.read_csv('MF_database.csv',encoding="utf-8",index_col='モンスター名')
 
 if st.button('ピクシー種'):
-    st.write('表示させるぞー！')
-#    Pixies=['ピクシー','ミント','スエコ','エンジェル','リリム']
-#    stock=st.selectbox(label='ピクシー種一覧',
-#                       options=Pixies)
-#    df_tgt=df.loc[stock]
-#    st.dataframe(df_tgt)
+    Pixies=['ピクシー','ミント','スエコ','エンジェル','リリム']
+    stock=st.selectbox(label='ピクシー種一覧',
+                       options=Pixies)
+    df_tgt=df.loc[stock]
+    st.dataframe(df_tgt)
