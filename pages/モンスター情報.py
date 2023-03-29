418b45fc6520c6e68f47b6d148b1092d4d4512b2
sana4950 <shalulu853@gmail.com>
2023-03-30 00:21:33 +0900

    Change15
    エラー修正

--- pages/モンスター情報.py
+++ pages/モンスター情報.py
@@ -8,9 +8,9 @@ df=pd.read_csv('MF_database.csv',encoding="utf-8",index_col='モンスター名'
 
 #ピクシー種ボタン作成していく
 if st.button(label='ピクシー種'):
-    family=df(df.at['メイン']=='ピクシー')
+    family=df.at['メイン']=='ピクシー'
     stock=st.selectbox(label='ピクシー種一覧',options=family)
-    tgt=df.loc(stock)
+    tgt=df.loc[stock]
 
     #status_1
     st.write(tgt.filter(items=stocks,axis=0).filter(items=['レア','メイン','サブ']))
