717a8d5534bad1163e7c912aa8d51e8a60bae950
sana4950 <123571065+sana4950@users.noreply.github.com>
2023-03-30 11:22:22 +0900

    Update14
    13がうまくいったので表示に調整を加える

--- pages/モンスター情報.py
+++ pages/モンスター情報.py
@@ -10,15 +10,16 @@ df=pd.read_csv('MF_database.csv',index_col='モンスター名')
 if st.button(label='ピクシー種'):
     family=['ピクシー','ミント','スエコ']
     stock=st.selectbox(label='ピクシー種一覧',options=family)
-    #tgt=df.loc[stock]失敗したら戻す
 
+    #列でデータ分割表示
+    cols=st.columns(2)
+    
+    with cols[0].container():
     #status_1
-    with st.container():
         st.dataframe(df.loc[stock].filter(items=['レア','メイン','サブ']))
     #status_2
-    with st.container():
         st.dataframe(df.loc[stock].filter(items=['ライフ','ちから','かしこさ','命中','回避','丈夫さ']))
     #status_3
-    with st.container():
+    with cols[1].container():
         st.dataframe(df.loc[stock].filter(items=['初期技1','初期技2']))
     #status_4
