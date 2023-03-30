26fa1180ad07d1d0e45f314912ad31c97c51e4d0
sana4950 <123571065+sana4950@users.noreply.github.com>
2023-03-30 15:42:00 +0900

    Update20
    エラー修正

--- pages/モンスター情報.py
+++ pages/モンスター情報.py
@@ -12,24 +12,24 @@ df=pd.read_csv('MF_database.csv',index_col='モンスター名')
 family_select=st.button(label='ピクシー種')
 if family_select:
     if 'push_1' not in st.session_state:
-        st.session_state.push_1=0
+        st.session_state.push_1=False
     #select作成
     stock=st.selectbox(label='ピクシー種一覧',options=family)
     if stock:
-        st.session_state.push_1=1
+        st.session_state.push_1=True
     #Trueなら以下処理に入る
-    if st.session_state.push_1=1:
-        #列でデータ分割表示
-        cols=st.columns(2)
-        #左列表示
-        with cols[0].container():
-            #status_1
-            st.write(df.loc[stock].filter(items=['レア','メイン','サブ']))
-            #status_2
-            st.write(df.loc[stock].filter(items=['ライフ','ちから','かしこさ','命中','回避','丈夫さ']))
-        #右列表示
-        with cols[1].container():
-            #status_3
-            st.write(df.loc[stock].filter(items=['初期技1','初期技2']))
-            #status_4
-            st.write(df.loc[stock].filter(items=['登録可能技1','登録可能技2']))
+        if st.session_state.push_1:
+            #列でデータ分割表示
+            cols=st.columns(2)
+            #左列表示
+            with cols[0].container():
+                #status_1
+                st.write(df.loc[stock].filter(items=['レア','メイン','サブ']))
+                #status_2
+                st.write(df.loc[stock].filter(items=['ライフ','ちから','かしこさ','命中','回避','丈夫さ']))
+            #右列表示
+            with cols[1].container():
+                #status_3
+                st.write(df.loc[stock].filter(items=['初期技1','初期技2']))
+                #status_4
+                st.write(df.loc[stock].filter(items=['登録可能技1','登録可能技2']))
