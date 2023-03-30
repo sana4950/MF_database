8e2c5ef466bf1c63a7d70d2705704d3987dfaaeb
sana4950 <123571065+sana4950@users.noreply.github.com>
2023-03-30 11:09:20 +0900

    Update13
    エラー修正

--- pages/モンスター情報.py
+++ pages/モンスター情報.py
@@ -10,15 +10,15 @@ df=pd.read_csv('MF_database.csv',index_col='モンスター名')
 if st.button(label='ピクシー種'):
     family=['ピクシー','ミント','スエコ']
     stock=st.selectbox(label='ピクシー種一覧',options=family)
-    tgt=df.loc[stock]
+    #tgt=df.loc[stock]失敗したら戻す
 
     #status_1
     with st.container():
-        st.dataframe(tgt.filter(items=['レア','メイン','サブ']))
+        st.dataframe(df[stock].filter(items=['レア','メイン','サブ']))
     #status_2
     with st.container():
-        st.dataframe(tgt.filter(items=['ライフ','ちから','かしこさ','命中','回避','丈夫さ']))
+        st.dataframe(df[stock].filter(items=['ライフ','ちから','かしこさ','命中','回避','丈夫さ']))
     #status_3
     with st.container():
-        st.dataframe(tgt.filter(items=['初期技1','初期技2']))
+        st.dataframe(df[stock].filter(items=['初期技1','初期技2']))
     #status_4
